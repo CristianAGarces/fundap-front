c37154a4d7680bb9bdf2d9837562b1734b11a5eb
FundapMacoe <fundapmacoe2025@gmail.com>
2025-06-08 14:50:05 -0500

    frontend ready

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import UserProfile from "@/components/User/User";
+import Link from "next/link";
 
 interface MainLayoutProps {
   children: React.ReactNode;
@@ -13,7 +14,14 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
       <div className="flex flex-col flex-1 overflow-hidden">
         <header className="h-16 bg-white shadow-sm z-10">
           <div className="flex items-center justify-between h-full px-6">
-            <UserProfile /> 
+            <Link
+              href="/"
+              className="size-40 bg-customYellow text-white text-xl hover:bg-customYellow/50 rounded-full flex items-center justify-center transition-colors duration-300"
+              title="Ir a inicio"
+            >
+              Ir a inicio
+            </Link>
+            <UserProfile />
           </div>
         </header>
         <main className="flex-1 overflow-x-auto overflow-y-auto p-6">
